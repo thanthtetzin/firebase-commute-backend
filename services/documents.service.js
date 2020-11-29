b740b90d3ee9471a83d421c2624138454c5dfbb7
thanthtetzin <thanthtetzin1990@gmail.com>
2020-11-29 19:58:07 +0700

    Commented unsure fix for orderByField notExitsted in doc

--- services/documents.service.js
+++ services/documents.service.js
@@ -5,9 +5,9 @@ const getDocument = exports.getDocument = async (collectionName, docId) => {
   const doc = await docRef.get();
   if (!doc.exists) {
     return null;
-  } else {
-    console.log(doc.data())
-    //return doc.data();
+  } 
+  else {
+    return doc.data();
   }
 }
 exports.searchDocuments = async searchParams => {
@@ -37,7 +37,7 @@ const replaceStartAfterStartAtWithActualDoc = async (searchParams) => {
   }
   if(searchParams.startAfter){
     console.log(searchParams.startAfter)
-    const doc = await getDocByDocIdAndApplyMockSortKeyValIfNotFound(searchParams.collectionName, searchParams.startAfter, searchParams.orderBy);
+    const doc = await getDocByDocIdForPagination(searchParams.collectionName, searchParams.startAfter, searchParams.orderBy);
     if(doc){
       searchParams.startAfter = doc;
       if(searchParams.startAt){
@@ -49,7 +49,7 @@ const replaceStartAfterStartAtWithActualDoc = async (searchParams) => {
     } 
   }
   if(searchParams.startAt){
-    const doc = await getDocByDocIdAndApplyMockSortKeyValIfNotFound(searchParams.collectionName, searchParams.startAt, searchParams.orderBy);
+    const doc = await getDocByDocIdForPagination(searchParams.collectionName, searchParams.startAt, searchParams.orderBy);
     if(doc){
       searchParams.startAt = doc;
       if(searchParams.startAfter){
@@ -64,14 +64,19 @@ const replaceStartAfterStartAtWithActualDoc = async (searchParams) => {
   return searchParams;
 }
 
-const getDocByDocIdAndApplyMockSortKeyValIfNotFound = async (collectionName, docId, orderBy) => {
+const getDocByDocIdForPagination = async (collectionName, docId, orderBy) => {
   const docRef = getDocumentRef(collectionName, docId);
   const doc = await docRef.get();
   
   if (doc.exists) {
-    if(!doc._fieldsProto[orderBy.fieldName]) {
-      doc._fieldsProto[orderBy.fieldName] = orderBy.mockValueIfNotFoundInDoc;
-    }
+    // Sometime orderByField is missing in the doc as it can be because of dirty data
+    // ** Sample Error Msg **
+    // Field “bookingDate” is missing in the provided DocumentSnapshot. Please provide a document that contains values for all specified orderBy() and where() constraints.
+    // ** Sample Error Msg **
+    // This is some kind of fix to put mock value in order to execute startAfter() startAt() query. (But not 100% sure)
+    // if(!doc._fieldsProto[orderBy.fieldName]) {
+    //   doc._fieldsProto[orderBy.fieldName] = orderBy.mockValueIfNotFoundInDoc;
+    // }
     return doc;
   } 
   else{
